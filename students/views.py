081b087619901d563a197d99f4063addceb65132
miletskiy <s.miletskiy@gmail.com>
2015-08-07 23:28:39 +0300

    Dobavil loop for i ispolsoval ego tri raza)) _ Vo views dobavil tuple iz dictionaries students

--- students/views.py
+++ students/views.py
@@ -1,33 +1,54 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
 # Views for students
 def students_list(request):
-	# return HttpResponse('<h1>Hello World</h1>') 
-	return render(request, 'students/students_list.html',{})
+    # return HttpResponse('<h1>Hello World</h1>') 
+    # return render(request, 'students/students_list.html',{})
+    students = (
+        {'id':1,
+        'first_name':u'Андрей',
+        'last_name':u'Корост',
+        'ticket':235,
+        'image': 'img/me.jpeg'},
+        {'id':2,
+        'first_name':u'Светлана',
+        'last_name':u'Ильдирова',
+        'ticket':2358,
+        'image': 'img/piv.png'},
+        {'id':3,
+        'first_name':u'Василий',
+        'last_name':u'Пупкин',
+        'ticket':2935,
+        'image': 'img/podoba3.jpg'},
+    )
+    return render(request, 'students/students_list.html',
+        {'students': students})
 
 # def students_edit(request):
-# 	return HttpResponse('<h1>Student Add Form232</h1>') 
+#   return HttpResponse('<h1>Student Add Form232</h1>') 
 
 def students_add(request):
-	return HttpResponse('<h1>Student Add Form</h1>') 
+    return HttpResponse('<h1>Student Add Form</h1>') 
 
 def students_edit(request,sid):
-	return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
 
 def students_delete(request,sid):
-	return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
 
 # Views for Groups
 def groups_list(request):
-	return HttpResponse('<h1>Groups Listing</h1>')
+    return HttpResponse('<h1>Groups Listing</h1>')
 # groups
 def groups_add(request):
-	return HttpResponse('<h1>Group Add Form</h1>')
+    return HttpResponse('<h1>Group Add Form</h1>')
 
 def groups_edit(request,gid):
-	return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
 
 def groups_delete(request,gid):
-	return HttpResponse('<h1>Delete Group %s</h1>' % gid)
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
