cceb1000a5e425d69c454d67aa500ee944dd2239
miletskiy <s.miletskiy@gmail.com>
2015-08-31 11:16:19 +0300

    Добавил мигации и восстановил sqlite3

---
+++ students/migrations/0006_auto_20150830_1732.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('students', '0005_student_student_group'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='group',
+            name='starosta',
+            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.SET_NULL, default='Localerror', to='students.Student', blank=True, verbose_name='\u0421\u0442\u0430\u0440\u043e\u0441\u0442\u0430'),
+        ),
+    ]
