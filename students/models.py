3c1e0154adca4580771c7c4bbf10a0bfa819c07f
miletskiy <s.miletskiy@gmail.com>
2015-08-31 09:31:10 +0300

    Добавил student_group и закоментил __unicode__

--- students/models.py
+++ students/models.py
@@ -49,11 +49,20 @@ class Student(models.Model):
     #     max_length = 6,
     #     )
 
-    def __unicode__(self):
-        return u"%s %s" % (self.first_name,self.last_name)
+    # def __unicode__(self):
+    #     return u"%s %s" % (self.first_name,self.last_name)
+    # def __unicode__(self):
+    #     return u"%s %s" % (unicode(self.first_name),unicode(self.last_name))
+
+    student_group = models.ForeignKey('Group',
+        verbose_name=u"Група",
+        blank=False,
+        null=True,
+        on_delete=models.PROTECT)
 
 class Group(models.Model):
     """Group Model"""
+
 
     class Meta(object):
         verbose_name=u"Група"
@@ -70,20 +79,23 @@ class Group(models.Model):
         blank=True,
         null = True,
         on_delete = models.SET_NULL,
-        default = u"Тут самоуправление :-)")
+        default = u"Localerror")
 
 
     notes = models.TextField(
         blank=True,
         verbose_name=u"Дополнительные заметки")
 
-    def __unicode__(self):
-        if self.starosta:
-            return u"%s (%s %s)" % (self.title,self.starosta.first_name,self.starosta.last_name)
-        else:
-            return u"%s" % (self.title,)
+    # def __unicode__(self):
+    #     # if self.starosta:
+    #     #     return u"%s (%s %s)" % (self.title,self.starosta.first_name,self.starosta.last_name)
+    #     # else:
+    #     #     return u"%s" % (self.title,)
 
-
+    #     if self.starosta:
+    #         return u"%s (%s %s)" % (unicode(self.title),unicode(self.starosta.first_name),unicode(self.starosta.last_name))
+    #     else:
+    #         return u"%s" % (unicode(self.title),)
 
 
 
